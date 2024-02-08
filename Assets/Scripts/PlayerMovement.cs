fb1284b63008067831abbb5f04c7af8dc9a04b5c
Pawel Czech <frognatic@gmail.com>
2024-02-08 10:33:52 +0100

    Small code cleanup.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 20f;
+
+    private float x;
+    private float z;
+    private Vector3 movement;
     
     private void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(x, 0, z);
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        movement = new Vector3(x, 0, z);
         
         transform.Translate(movement * speed * Time.deltaTime);
     }
